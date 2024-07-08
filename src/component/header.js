a02888ee871ea84e1c38d31adf8a4655d1e7b2e7
ATLP <atlp@ATLPs-MacBook-Pro.local>
2024-07-08 12:43:34 +0200

    login

--- src/component/header.js
+++ src/component/header.js
@@ -3,21 +3,34 @@ import React, { useState } from "react";
 import "../sass/style.css"
 import StarLogo from "./logo";
 import { MenuOutlined } from '@ant-design/icons';
+import { CloseOutlined } from '@ant-design/icons';
 
 
 export default function Navigation(){
     const  [menu,setMenu]=useState(false)
+    const [hidicon,sethideicon]=useState(true)
+    const [close,setcloseicon]=useState(false)
 
     const handlemenu = ()=>{
         setMenu(true)
+        sethideicon(false)
+        setcloseicon(true)
+    }
+    const Hidenavigate = () =>{
+        setMenu(false)
+        sethideicon(true)
+        setcloseicon(false)
     }
     return (
         <>
            <nav className={menu=== true ? 'navrespone':''}>
-        <div class="logo">
-            < StarLogo className='head-logo'/>
-            <h3>UI/XI</h3>
-        </div>
+             <div className="combine-log">
+                 <div class="logo">
+                  < StarLogo className='head-logo'/>
+                   <h3>UI/XI</h3>
+                </div>
+                {close && (<CloseOutlined className="menu" onClick={Hidenavigate}/>)}
+            </div>
         <div class="link">
             <ul className={menu=== true ? 'navlinks':'links'}>
                 <li><a href="./#home" class="nav-links">Home</a></li>
@@ -28,9 +41,11 @@ export default function Navigation(){
                 <li><a href="./#contact" class="nav-links">Contact Me</a></li>
                 <li><a href="login" class="nav-links btn btn-green">Login</a></li>
             </ul>
-            <div className="respo" onClick={handlemenu}>
-            <MenuOutlined className="menu"/>
-            </div>
+            {hidicon && (
+                <div className="respo" onClick={handlemenu}>
+                <MenuOutlined className="menu"/>
+                </div>
+            )}
 
         </div>
     </nav>
